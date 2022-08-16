f522ef62fad8775af74936ad6ba95c8143eb3935
bally <108769285+ballyalley-o@users.noreply.github.com>
2022-08-17 02:01:19 +1200

    finishing

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
+
+
+const artvid = document.querySelectorAll('.artvid');
+for (let i = 0; i<artvid.length; i++) {
+    artvid[i].addEventListener('loadedmetadata', function(e) {
+        this.currentTime = 5;
+    }, false)
+    artvid[i].addEventListener('mouseenter', function(e) {
+        artvid[i].play()
+    })
+    artvid[i].addEventListener('mouseout', function(e) {
+        artvid[i].pause()
+
+    })
+}
